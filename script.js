5ac35197ac5cc0b020cc8d5b29af0d6f4757a92b
OskaruSenpai <ovoorel@gmail.com>
2017-06-06 19:05:30 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,62 +1,62 @@
-$(document).ready(function(){
-    $("#info0").click(function(){
+$(document).ready(function() {
+    $("#info0").click(function() {
         $("#infoh1").text("Laste eraldamine lasteaiast nakkushaiguste tõttu");
         $("#infop").html("<strong>Normaalne väikelaps võib haigestuda tavalistesse viirusnakkustesse 5-10 korda aastas.</br></br></strong>Nakkushaiguste esinemine jääb tavaliselt sügise-, talve- ja kevadperioodi ja lastel esineb neid enam alates 9 elukuust kuni 1,5 aastani. Tavaliselt kestavad nakkused mõnest päevast kuni nädalani ja nendest toibumine võib võtta veel kuni nädala. Hingamisteede infektsioone põhjustab üle 100 erineva viiruse. Heal juhul haigestub laps väheste sümptoomidega või siis lausa sümptoomideta ehk asümptomaatiliselt. Kehvemini läheb lastel, kes haigestuvadpea igast kontaktist viirustega ja neil esinevad kaebused, need lapsed satuvad niinimetatud infektsiooni keerdkäiku.</br></br>Haigustekitajad satuvad haigelt tervele tavaliselt hingamisteede vahendusel, kuid ka väljaheite või põletikulise eksudaadi nagu röga või ninalima kaudu. See juhtub enamasti käte vahendusel. Suur osa nakkustekitajatest vajab nakkamiseks väga lähedast kontakti. Haigestuvus on madalam kodus kasvavatel lastel, suurem lasteaias käivatel lastel. <strong>	Enamus haigusi nakkab juba enne sümptomite ilmnemist, seetõttu on lasteaedades palju nakkust kandvaid lapsi juba enne kui esimene haige laps leitakse.</strong></br></br><strong>Laste eraldamine lasteaiast nakkushaiguste tõttu.</strong></br></br>Nakkushaiguste levikut ei aita tõrjuda haigete laste eraldamine lasteaiast. Enamasti on rühmas samaaegselt nii asümptomaatilisi lapsi, nakkuskandjaid, kes pole veel haigestunud, kui ka juba sümptomaatilisi lapsi ja nakkusest toibuvaid lapsi.</br></br>Haige lapse seisukohalt on siiski tähtis, et laps saaks põdeda ja haigusest toibuda kodus ja naasta lasteaeda alles siis, kui ta on võimeline osalema normaalses lasteaia päevarutiinis. Haigusest toibumine on väga individuaalne ja selleks ei saa anda üldisi soovitusi, soovitusi saab anda vaid konkreetsele lapsele.</br></br><strong>Kätepesu on tähtis ja seda ka kodus. Lapse käed peaks pesema ja kuivatama lasteaiast koju jõudes, enne sööki ja WC-s käimise järgselt.</strong></br></br>Järgnevalt mõned tavalisemad tõved ja nendest tingitud lasteaiast eraldamise aeg.</br></br><strong>Palavik</strong></br></br>Last ei viida lasteaeda kui palavik on üle 37,5 kraadi. Haigusest toibumine loetakse heaks kui laps on olnud palavikuvaba ühe ööpäeva.</br></br><strong>Köha ja nohu</strong></br></br>Ei ole põhjuseks lasteaiast eemale hoidmiseks kui ei esine muid nakkushaiguse sümptomeid ega häiri muul moel lapse üldseisundit.</br></br><strong>Gripilaadne nohuviirus</strong></br></br>Laps võib minna lasteaeda kui üldseisund ja palavik lubavad.</br></br><strong>Kõhulahtisus ja oksendamine</strong></br></br>Laps peab jääma koju kuni sümptomid lõppevad.</br></br><strong>Lööbelised haigused</strong></br></br>Haigus reeglina ei nakka enam peale lööbe ilmnemist. Lasteaiast eemaloleku vajaduse määrab lapse üldseisund.</br></br><strong>Tuulerõuged</strong></br></br>Eraldamine lasteaiast soovitatav 5 päeva lööbimisest, koorikute kuivamist ei pruugi oodata.</br></br><strong>Läkaköha</strong></br></br>Võib naasta lasteaeda 5 päeva peale ravi alustamist.</br></br><strong>Angiin</strong></br></br>Sõltub üldseisundist, ei ole vajadust eraldamiseks üle 24 tunni.</br></br><strong>Kõrvapõletik</strong></br></br>Ei vaja eraldamist. Sõltub lapse üldseisundist.</br></br><strong>Naha mädapõletik (Impetigo)</strong></br></br>Eraldamine vajalik 24 tundi alates suukaudse ravi alustamisest.</br></br><strong>Viirustüükad (molluscumid)</strong></br></br>Ei vaja eraldamist.</br></br><strong>Silmapõletik</strong></br></br>Ei vaja eraldamist kuna tekitajad on samad, mis hingamisteede põletike puhul.</br></br><strong>Enterobiaas ehk naaskelsabatõbi</strong></br></br>Eraldamine ei ole vajalik, vajalik kogu pere ravi samaaegselt.</br></br><strong>Sügelised ja täid</strong></br></br>Eraldamine vajalik kuni lapson ravitud.");
-});
-$("#info1").click(function(){
+    });
+    $("#info1").click(function() {
         $("#infoh1").text("Valmistumine vereprooviks");
         $("#infop").html("- Andke vereprooviks veri hommikul enne kella 11:30, parim ajavahemik on</br> 8:00 - 9:00.</br>- Enne vereandmise protseduuri peate olema vähemalt 1 tund ärkvel.</br>- Vereandmisele eelneval päeval võite süüa ja juua nagu tavaliselt, kuid piirake alkoholi, kohvi ja rasvase toidu tarbimist.</br>- 10-14 tundi enne vereandmist ärge sööge ja jooge. Vajadusel võite selle aja jooksul juua ühe klaasi puhast maitsestamata gaasita vett.</br>- Vereandmise päeva hommikul vältige enne protseduuri:</br> &nbsp&nbsp&nbsp - füüsilist ja emotsionaalset koormust,</br>&nbsp&nbsp&nbsp - võimalusel ravimite kasutamist,</br> &nbsp&nbsp&nbsp - suitsetamist.</br>- Enne vereandmise protseduuri peate vereringe stabiliseerumiseks vähemalt 15 minutit rahulikult istuma.</br>- Protseduuri ajal peab Teie suu olema tühi: ei tohi süüa, juua ega närida nätsu.");
-});
-$("#info2").click(function(){
+    });
+    $("#info2").click(function() {
         $("#infoh1").text("Uriiniproovi võtmine");
         $("#infop").html("- Uriini analüüsiks võtke hommikune ärkamise järgne uriin.</br>- 10-14 tundi enne uriiniproovi võtmist ärge sööge ja jooge. Vajadusel võite selle aja jooksul juua kuni 1 klaas puhast maitsestamata gaasita vett.</br>- Proovivõtu eelselt peab uriin olema põies olnud 4-6 tundi ehk eelmisest urineerimisest peab olema möödas vähemalt 4-6 tundi.</br>- Peske enne urineerimist oma välissuguelundid sooja voolava veega. Ärge kasutage mingeid pesemisvahendeid (seepi, nuustikut jm). Oluline on puhtaks pesta kusiti välissuue. Selleks tuleb naistel häbememokad sõrmedega laiali lükata, meestel eesnahk tagasi tõmmata.</br>- Kuivatage välissuguelundid kergelt paberkäterätikuga.</br>- Jälgige, et kusiti välissuue jääks proovivõtmiseks paljastatuks.</br>- Kui naistel esineb tupevoolust või menstruaalvere eritust, siis tuleb kasutada tampooni.</br>- Proovi võtmiseks alustage urineerimist WC potti. Mõne milliliitri urineerimise järgselt uriinijuga katkestamata koguge uriin (umbes 50ml ehk kolmveerand proovinõud) proovinõusse. Lõpetage urineerimine WC potti.- Uriiniproovi jaoks mõeldud proovinõusid saate apteegist või küsida perearstikeskusest. Käepäraste koduste proovinõude kasutamine ei ole lubatud.</br>- Võtke proovi jaoks uriin otse proovinõusse. Uriini ümbervalamine ühest nõust teise ei ole lubatud.</br>- Peale uriiniproovi võtmist sulgege proovinõu korralikult kaanega.</br>- Kirjutage proovinõu küljele oma nimi ja proovivõtmise kuupäev ning kellaaeg.");
-});
-$("#info3").click(function(){
+    });
+    $("#info3").click(function() {
         $("#infoh1").text("Roojaproovi võtmine");
         $("#infop").html("- Kasutage proovivõtmiseks spetsiaalset lusikaga proovinõud. Neid saate küsida apteegist või perearstikeskusest.</br>- Roe ei tohi kokku puutuda uriiniga, WC-poti loputusveega ning puhastuskemikaalidega.</br>- Võtke prooviks värsket rooja 1-2 grammi ehk üks proovinõu lusikatäis. Proovimaterjali tuleb võtta roojaportsjoni erinevatest kohtadest.</br>- Asetage lusikas koos roojaga proovinõusse.</br>- Sulgege proovinõu korralikult.</br>- Kirjutage proovinõu küljele oma nimi ja proovivõtmise kuupäev ning kellaaeg.");
-});
-$("#info4").click(function(){
+    });
+    $("#info4").click(function() {
         $("#infoh1").text("Hinnakiri");
-        $("#infop").html("<a href='http://www.maheperearst.ee/index.php?picfile=6'> http://www.maheperearst.ee/index.php?picfile=6</a>");
-});
-$("#arsti0").click(function(){
+        $("#infop").html("<a href='http://www.maheperearst.ee/index.php?picfile=7'> HINNAKIRI alates 01.04.2017</a>");
+    });
+    $("#arsti0").click(function() {
         $("#arstih1").text("Dr. Vanda Kristjan");
         $("#arstip").html("Vastuvõtule tuleb registreeruda telefonil <strong>623 8002</strong><strong></br>E  &nbsp&nbsp&nbsp&nbsp   9 - 13</strong></br><strong>T   &nbsp&nbsp&nbsp&nbsp  14 - 18</strong></br><strong>K   &nbsp&nbsp&nbsp&nbsp  12 - 16 (terved imikud)</strong></br><strong>N    &nbsp&nbsp&nbsp&nbsp   8 - 12</strong></br><strong>R   &nbsp&nbsp&nbsp&nbsp  12 - 16</strong>");
-});
-$("#arsti1").click(function(){
+    });
+    $("#arsti1").click(function() {
         $("#arstih1").text("Dr. Arved Kristjan");
         $("#arstip").html("Vastuvõtule tuleb registreeruda telefonil <strong>623 8002</strong></br><strong>E  &nbsp&nbsp&nbsp&nbsp   9 - 13</strong></br><strong>T   &nbsp&nbsp&nbsp&nbsp  14 - 18</strong></br><strong>K   &nbsp&nbsp&nbsp&nbsp  12 - 16 (terved imikud)</strong></br><strong>N    &nbsp&nbsp&nbsp&nbsp   8 - 12</strong></br><strong>R   &nbsp&nbsp&nbsp&nbsp  12 - 16</strong>");
-});
-$("#arsti2").click(function(){
+    });
+    $("#arsti2").click(function() {
         $("#arstih1").text("Dr. Tiina Saks");
         $("#arstip").html("Vastuvõtule tuleb registreeruda telefonil <strong>623 8002</strong>");
-});
-$("#Vaktsineerimine0").click(function(){
+    });
+    $("#Vaktsineerimine0").click(function() {
         $("#Vaktsineerimineh1").text("Imikute ja väikelaste plaaniline vaktsineerimine");
         $("#Vaktsineeriminep").html('Teostame kõiki riiklikus immuniseerimiskavas nimetatud vaktsineerimisi. <a href="https://www.riigiteataja.ee/akt/107072011003%22%3E https://www.riigiteataja.ee/akt/107072011003</a> .</br> <p>Tervete imikute ja väikelaste vaktsineerimine toimub kolmapäeviti ja neljapäeviti. Täpse aja kokkuleppimiseks tuleks helistada telefonil <strong>623 8002</strong>');
-});
-$("#Vaktsineerimine1").click(function(){
+    });
+    $("#Vaktsineerimine1").click(function() {
         $("#Vaktsineerimineh1").text("Täiendav vabatahtlik vaktsineerimine");
         $("#Vaktsineeriminep").html("Teostame järgmiste haigustekitajate vastaseid vaktsineerimisi: - puukentsefaliidiviirus</br>- gripiviirus (<em>Influenzae</em>)</br> - pneumokokk (<em>Streptococcus pneumonia</em>)</br>   - A-hepatiidiviirus</br>   - B-hepatiidiviirus</br>   - tuulerõugeviirus (<em>Varicella</em>)</br>   - papilloomiviirus (HPV)</br></br>");
-});
-$("#toendid0").click(function(){
+    });
+    $("#toendid0").click(function() {
         $("#toendidh1").text("Juhiloa tervisetõend");
         $("#toendidp").html("Väljastame juhiloa tervisetõendit meie keskuse nimistute patsientidele.</br></br>Tervisetõendi taotlemiseks helistage palun telefonil</br> <strong>623 8002</strong> ja leppige pereõega aeg kokku.</br></br>Tervisetõend on tasuline ja maksab :</br>- <strong>20 eurot</strong>, kui on täidetud e-tervisedeklaratsioon patsiendiportaalis,</br>- <strong>30 eurot</strong>, kui tervisedeklaratsioon on täidetud paberil.");
-});
-$("#toendid1").click(function(){
+    });
+    $("#toendid1").click(function() {
         $("#toendidh1").text("Toidukäitleja tervisetõend");
         $("#toendidp").html("Väljastame juhiloa tervisetõendit meie keskuse nimistute patsientidele.</br></br>Tervisetõendi taotlemiseks helistage palun telefonil</br> <strong>623 8002</strong> ja leppige pereõega aeg kokku.</br></p>Tervisetõend on tasuline ja maksab <strong>45 eurot</strong>.");
-});
-$("#toendid2").click(function(){
+    });
+    $("#toendid2").click(function() {
         $("#toendidh1").text("Tervisetõend Kaitseväe arstlikule komisjonile");
         $("#toendidp").html("Tervisetõendi vormistamiseks kaitseväe arstlikule komisjonile tuleb helistada telefonil <strong>623 8002 </strong>ja broneerida aeg arsti vastuvõtule.</br></br>Tervisetõend on kutsealusele tasuta.");
-});
-$("#toendid3").click(function(){
+    });
+    $("#toendid3").click(function() {
         $("#toendidh1").text("Relvaloa tervisetõend");
         $("#toendidp").html("Väljastame juhiloa tervisetõendit meie keskuse nimistute patsientidele.</br></br>Tervisetõendi taotlemiseks tuleb käia psühhiaatri vastuvõtul. Selleks vajalike paberite vormistamiseks palun helistage telefonil</br> <strong>623 8002 </strong> ja leppige pereõega aeg kokku.</br></br>Tervisetõend on tasuline ja maksab <strong>20 eurot</strong>.");
-});
-$("#toendid4").click(function(){
+    });
+    $("#toendid4").click(function() {
         $("#toendidh1").text("Muud tervisetõendid ");
         $("#toendidp").html("Muudel eesmärkidel väljastatud tervisetõend maksab <strong>20 eurot</strong>.</br></br>Väljastame tervisetõendeid töökohale, kindlustusele, spordiklubidele ja vajadusel muul eesmärgil.</br></br>Klienditeenindaja tervisetõendi taotlemiseks tuleb teha rindkere röntgenuuring. Saatekirja saamiseks palun helistage telefonil</br> <strong>623 8002 </strong> ja leppige pereõega aeg kokku.");
-});
+    });
 });
